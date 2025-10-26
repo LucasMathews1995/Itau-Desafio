fad2e790765a6eb684e2e6424f4c12dd59a8dfda
Lucas Mathews <lucasmathews50@gmail.com>
2025-10-26 08:01:47 -0300

    first-commit

---
+++ src/main/java/desafio/itau/springboot/dto/TransactionRequest.java
@@ -0,0 +1,29 @@
+package desafio.itau.springboot.dto;
+
+import java.time.OffsetDateTime;
+
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+
+public class TransactionRequest {
+
+    @NotNull
+   
+private Double valor;
+
+@NotNull
+private OffsetDateTime dataHora;
+
+
+
+
+
+public Double getValor() {
+    return valor;
+}
+
+
+public OffsetDateTime getDataHora() {
+    return dataHora;
+}
+}
